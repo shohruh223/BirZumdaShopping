612546ade1e7e58d493983b8cbc5f21668f38725
Shohruh223 <shohruh.abd2223@gmail.com>
2026-04-18 09:21:46 +0500

    bugun yangi kodlar (16.04.2026)

--- root/settings.py
+++ root/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n8#g(ox5-8ok90zgrbtc_yzrr%tws%uz!(zi=4pzdkzwd^3s_('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
